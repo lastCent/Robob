7ee6276966b4bd75fedacdde8d94a6b47811cf8b
lastCent <richard.bman@gmail.com>
2018-02-25 17:26:57 +0100

    Claw tweak

--- Robobob_claws/Robobob_claws.ino
+++ Robobob_claws/Robobob_claws.ino
@@ -16,10 +16,12 @@ Config
 const int transistor_pin = 12;    	// Shock weapon transistor gate
 const int shock_PWM_pin = 9;         	// Shock PWM input-pin
 const int servo_PWM_pin = 10; 		// Servo PWM input-pin
-const int servo_ctrl_pin_l = 3;		// Left servo control
-const int servo_ctrl_pin_r = 5;		// Right servo control
-const int servo_start_deg = 0;		// Claws neutral position
-const int servo_close_deg = 90;		// Claws closed position
+const int servo_ctrl_pin_l = 5;		// Left servo control
+const int servo_ctrl_pin_r = 3;		// Right servo control
+const int servo_l_open = 80;
+const int servo_l_closed = 5;
+const int servo_r_open = 0;
+const int servo_r_closed = 82;
 const int toggle_threshold = 1500;	// PWM threshold of toggling signal
 const int safety_delay = 1000;		// Delay before shock, in millis
 //-----------------------------------------------------------------------------
@@ -36,6 +38,15 @@ void setup() {
     pinMode(servo_PWM_pin, INPUT);
     servo_l.attach(servo_ctrl_pin_l);
     servo_r.attach(servo_ctrl_pin_r);
+    // Dab sequence
+    servo_l.write(servo_l_open); 
+    servo_r.write(servo_r_closed);
+    delay(1000);
+    servo_l.write(servo_l_closed);
+    servo_r.write(servo_r_open);
+    delay(1000);
+    servo_l.write(servo_l_open);
+    servo_r.write(servo_r_open);
     Serial.begin(115200);
     Serial.println("Setup Complete");       //DEBUG
 }
@@ -64,15 +75,15 @@ int do_servos() {
     int servo_PWM = pulseIn(servo_PWM_pin, HIGH);
     //Serial.println(servo_PWM);             //DEBUG
     if (!claws_closed && servo_PWM > toggle_threshold) {
-        servo_l.write(servo_close_deg);
-	      servo_r.write(servo_start_deg);
+        servo_l.write(servo_l_closed);
+        servo_r.write(servo_r_closed);   
         last_toggle = millis();
         claws_closed = true;   
         Serial.println("Claws closed");         //DEBUG
         return 1;
     } else if (claws_closed && servo_PWM < toggle_threshold) {
-        servo_l.write(servo_start_deg);
-        servo_r.write(servo_close_deg);
+        servo_l.write(servo_l_open);
+        servo_r.write(servo_r_open);
         last_toggle = millis();
         claws_closed = false;
         Serial.println("Claws opened");         //DEBUG
