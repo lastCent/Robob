f69342435c78430bb2adf021b5326117d51e69fc
lastCent <richard.bman@gmail.com>
2018-02-27 12:32:54 +0100

    Advanced steering rework

--- BattleBotBrainV3Advanced/BattleBotBrainV3Advanced.ino
+++ BattleBotBrainV3Advanced/BattleBotBrainV3Advanced.ino
@@ -1,6 +1,10 @@
 // Advanced battle bot wheel control.
 // Debug serial output left on, since some tweaking may be needed for redeployment 
 
+// TODO: SEPARATE SHIT, MAKE CONFIG SECTION
+// UNFUCK VARIABLE NAMES
+// 4 space indent
+
 // Forward-Backward steering
 #define THROTTLE_SIGNAL_IN_A 0		// INTERRUPT 0 = DIGITAL PIN 2 - use the interrupt number in attachInterrupt
 #define THROTTLE_SIGNAL_IN_A_PIN 11	// INTERRUPT 0 = DIGITAL PIN 11 - use the PIN number in digitalRead
@@ -19,100 +23,144 @@ volatile int nThrottleInB = NEUTRAL_THROTTLE_B;	// volatile, we set this in the 
 volatile unsigned long ulStartPeriodB = 0;	// set in the interrupt
 volatile boolean bNewThrottleSignalB = false;	// set in the interrupt and read in the loop
 
+// Flags
+bool forward, backward, left, right; 
 // Wheel variables
-int throttle;
-int engineSpeed;
-const float baseSteerPercent = 0.5;	// Go straight here
-float steerPercent; 			// 1 Goes right, 0 left
-const int maxForward = 1116; 		// Must be tweaked
-const int maxBack = 1792;    		// Use control to test values
-const int maxLeft = 1048 ;   		// and find values from the Serial Monitor
+float throttlePercent;
+float steerPercent;                     // How much left or right robot should go
+const int maxForward = 1116;            // Must be tweaked
+const int maxBack = 1792;               // Use control to test values
+const int maxLeft = 1048 ;              // and find values from the Serial Monitor
 const int maxRight = 2000;
 
 const int IN_1=5;  	// HBridge IN_1
 const int IN_2=6;  	// HBridge IN_2
 const int IN_3=7;  	// HBridge IN_3
 const int IN_4=8;  	// HBridge IN_4
-const int pinPWM_A=9; 	// HBridge EN_A
-const int pinPWM_B=10;  // HBrigde EN_B
+const int pinPWM_A=9; 	// HBridge EN_A  (venstre?)
+const int pinPWM_B=10;  // HBrigde EN_B (høyre?)
 
 void setup() {
-// tell the Arduino we want the function calcInput to be called whenever INT0 (digital pin 2)
-// changes from HIGH to LOW or LOW to HIGH
-// catching these changes will allow us to calculate how long the input pulse is
-attachInterrupt(digitalPinToInterrupt(THROTTLE_SIGNAL_IN_A_PIN),calcInputA,CHANGE);
-
-delay(10); // allows for Interrupts and I can afford to wait 100th of second to process interrupts
-
-// tell the Arduino we want the function calcInput to be called whenever INT0 (digital pin 3)
-// changes from HIGH to LOW or LOW to HIGH
-// catching these changes will allow us to calculate how long the input pulse is
-attachInterrupt(digitalPinToInterrupt(THROTTLE_SIGNAL_IN_B_PIN),calcInputB,CHANGE);
-
-pinMode(IN_1,OUTPUT);
-pinMode(IN_2,OUTPUT);
-pinMode(IN_3,OUTPUT);
-pinMode(IN_4,OUTPUT);
-pinMode(pinPWM_A,OUTPUT);
-pinMode(pinPWM_B,OUTPUT);
-Serial.begin(9600);
+    // tell the Arduino we want the function calcInput to be called whenever INT0 (digital pin 2)
+    // changes from HIGH to LOW or LOW to HIGH
+    // catching these changes will allow us to calculate how long the input pulse is
+    attachInterrupt(digitalPinToInterrupt(THROTTLE_SIGNAL_IN_A_PIN),calcInputA,CHANGE);
+    
+    delay(10); // allows for Interrupts and I can afford to wait 100th of second to process interrupts
+    
+    // tell the Arduino we want the function calcInput to be called whenever INT0 (digital pin 3)
+    // changes from HIGH to LOW or LOW to HIGH
+    // catching these changes will allow us to calculate how long the input pulse is
+    attachInterrupt(digitalPinToInterrupt(THROTTLE_SIGNAL_IN_B_PIN),calcInputB,CHANGE);
+    
+    pinMode(IN_1,OUTPUT);
+    pinMode(IN_2,OUTPUT);
+    pinMode(IN_3,OUTPUT);
+    pinMode(IN_4,OUTPUT);
+    pinMode(pinPWM_A,OUTPUT);
+    pinMode(pinPWM_B,OUTPUT);
+    Serial.begin(9600);
+    forward, backward, left, right = false;
 }
 
 void loop () {
-  delay(20); // For testing
-  //Handle new speed value
-  if (bNewThrottleSignalA) {
-    if (nThrottleInA > NEUTRAL_THROTTLE_A-100 && nThrottleInA < NEUTRAL_THROTTLE_A+100) {
-      //NO FORWARD/BACKWARD
-      throttle = NEUTRAL_THROTTLE_A;
-    }
-    else {
-      throttle = nThrottleInA;
-    }
+    delay(50); // For testing
+    //Handle new speed value
+    handleThrottle();
+    //Handle new turn value
+    handleTurn();
+    // Write new values to engines
     updateSpeeds();
-    bNewThrottleSignalA = false;
-  }
-  //Handle new turn value
-  if (bNewThrottleSignalB) {
-    if (nThrottleInB > NEUTRAL_THROTTLE_B-10 && nThrottleInB < NEUTRAL_THROTTLE_B+10) {
-      // NO LEFT OR RIGHT
-      steerPercent = 0.5;
-    }
-    else {
-      steerPercent = (float)(nThrottleInB - maxLeft)/(maxRight - maxLeft);
-    }
-    updateSpeeds();
-    bNewThrottleSignalB = false;
-  }
 }
+
+void handleThrottle() {
+    if (bNewThrottleSignalA) {
+        if (nThrottleInA > NEUTRAL_THROTTLE_A-100 && nThrottleInA < NEUTRAL_THROTTLE_A+100) {
+            //NO FORWARD/BACKWARD
+            throttlePercent = 0;
+            forward, backward = false;
+        }
+        else if (nThrottleInA < NEUTRAL_THROTTLE_A){
+            // Going Forward
+            throttlePercent = ((NEUTRAL_THROTTLE_A-nThrottleInA)/(NEUTRAL_THROTTLE_A-maxForward));
+            forward = true; 
+            backward = false;
+        } else {
+            // Going Backward
+            throttlePercent = ((NEUTRAL_THROTTLE_A-nThrottleInA)/(NEUTRAL_THROTTLE_A-maxBack));
+            backward = true;
+            forward = false;
+        }
+    bNewThrottleSignalA = false;
+    }    
+}
+
+void handleTurn() {
+  if (bNewThrottleSignalB) {
+    if (nThrottleInB > NEUTRAL_THROTTLE_B-100 && nThrottleInB < NEUTRAL_THROTTLE_B+100) {
+      // NO LEFT OR RIGHT
+      steerPercent = 0;
+      left, right = false;
+    } else if (nThrottleInB < NEUTRAL_THROTTLE_B) {
+      // Going left 
+      steerPercent = (float)(NEUTRAL_THROTTLE_B-nThrottleInB)/(NEUTRAL_THROTTLE_B-maxLeft);
+      left = true;
+      right = false;
+    } else {
+      //Going Right 
+      steerPercent =(float)(NEUTRAL_THROTTLE_B-nThrottleInB)/(NEUTRAL_THROTTLE_B-maxRight) ; 
+      right = true;
+      left = false; 
+    }
+    bNewThrottleSignalB = false;
+  }    
+}
+    
 void updateSpeeds() {
-  // Forward:
-  if (throttle < NEUTRAL_THROTTLE_A) {
-    digitalWrite(IN_1, HIGH);
-    digitalWrite(IN_2, LOW);
-    digitalWrite(IN_3, HIGH);
-    digitalWrite(IN_4, LOW); 
-    engineSpeed = 255*((NEUTRAL_THROTTLE_A-throttle)/(NEUTRAL_THROTTLE_A-maxForward));
+  // Stand in place (and turn)
+  if (!(forward || backward)) {
+    // Do nothing
+    if (!(left || right)) {
+      setHBridge(LOW, LOW, LOW, LOW, 0, 0);
+    }
+    // Turn right
+    else if (right) {
+      setHBridge(LOW, LOW, LOW, LOW, 255*steerPercent, 255*steerPercent);
+    }
+    // Turn left
+    else if (left){
+      setHBridge(HIGH, HIGH, HIGH, HIGH,255*steerPercent, 255*steerPercent);  
+    }
   }
-  // Backward
+  // Move forward
+  else if (forward) {
+    // Straight
+    if (!(left || right)) {
+        setHBridge(HIGH, LOW, HIGH, LOW, 255*throttlePercent,255*throttlePercent);
+    }
+    // Right
+    else if (right) {
+      setHBridge(HIGH, LOW, HIGH, LOW, 255*throttlePercent, 255*throttlePercent*(1-steerPercent));
+    } 
+    // Left
+    else if (left) {
+      setHBridge(HIGH, LOW, HIGH, LOW, 255*throttlePercent*(1-steerPercent), 255*throttlePercent);  
+    }
+  }
+  // Move backward
   else {
-    digitalWrite(IN_1, LOW);
-    digitalWrite(IN_2, HIGH);
-    digitalWrite(IN_3, LOW);
-    digitalWrite(IN_4, HIGH);
-    engineSpeed = 255*((NEUTRAL_THROTTLE_A-throttle)/(NEUTRAL_THROTTLE_A-maxBack));
-  }
-  // Right
-  if (steerPercent > 0.5) {
-    // Normal driving operation
-    analogWrite(pinPWM_A, engineSpeed*(1-steerPercent));
-    analogWrite(pinPWM_B, engineSpeed);
-  }
-  // left
-  else {
-    // Normal driving operation
-    analogWrite(pinPWM_A, engineSpeed);
-    analogWrite(pinPWM_B, engineSpeed*steerPercent);
+    // Straight
+    if (!(left || right)) {
+        setHBridge(LOW, HIGH, LOW, HIGH, 255*throttlePercent, 255*throttlePercent);
+    }
+    // Right
+    else if (right) {
+        setHBridge(LOW, HIGH, LOW, HIGH, 255*throttlePercent, 255*throttlePercent*(1-steerPercent));
+    }
+    // Left
+    else if (left) {
+        setHBridge(LOW, HIGH, LOW, HIGH, 255*throttlePercent*(1-steerPercent), 255*throttlePercent);  
+    }
   }
 }
 
@@ -146,4 +194,14 @@ void calcInputB() {
     }
   }
 }
+
+void setHBridge(uint8_t one, uint8_t two, uint8_t three, uint8_t four, uint8_t A, uint8_t B) {
+    digitalWrite(IN_1, one);
+    digitalWrite(IN_2, two);
+    digitalWrite(IN_3, three);
+    digitalWrite(IN_4, four);
+    analogWrite(pinPWM_A, A);
+    analogWrite(pinPWM_B, B);
+}
+
 
