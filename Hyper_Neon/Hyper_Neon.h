0a3e7ac1eebcee4169f54cb8b7ff83afd50d6b68
lastCent <richard.bman@gmail.com>
2019-03-23 13:39:04 +0100

    Changed interrupt checking to be more responsive

--- Hyper_Neon/Hyper_Neon.h
+++ Hyper_Neon/Hyper_Neon.h
@@ -1,4 +1,40 @@
-/* Header file for Hyper_Neon */
+/* Header file for Hyper_Neon 
+ * ---------------------------------------------------------------------------------
+ */
+
+// Output pins
+#define weapon_pin 6
+#define t_pin_red 9
+#define t_pin_green 10
+#define t_pin_blue 11
+#define onboard_led_pin 13 // For debugging purposes
+
+// Weapon related values
+#define WEAPON_SIGNAL_IN_A 1       // INTERRUPT 0 = DIGITAL PIN 2 - use the interrupt number in attachInterrupt
+#define WEAPON_SIGNAL_IN_A_PIN 2   // INTERRUPT 0 = DIGITAL PIN 11 - use the PIN number in digitalRead#define NEUTRAL_THROTTLE_A 1460      // this is the duration in microseconds of neutral throttle on an electric RC Car
+#define NEUTRAL_THROTTLE_A 1460      // this is the duration in microseconds of neutral throttle on an electric RC Car
+#define maxForward 1144
+#define maxBack 1820 
+#define DEADZONE_A 200
+
+// LED related values
+#define LED_SIGNAL_IN_B 0          // INTERRUPT 1 = DIGITAL PIN 3 - use the interrupt number in attachInterrupt
+#define LED_SIGNAL_IN_B_PIN 3      // INTERRUPT 1 = DIGITAL PIN 3 - use the PIN number in digitalRead
+#define NEUTRAL_THROTTLE_B 1212             // This value may require changing the DX4e (Reading 1420 as neutral
+#define maxLeft 868
+#define maxRight 1760
+#define DEADZONE_B 200
+
+// Forward-Backward steering
+volatile int nThrottleInA = NEUTRAL_THROTTLE_A;    // volatile, we set this in the Interrupt and read it in loop so it must be declared volatile
+volatile unsigned long ulStartPeriodA = 0;  // set in the interrupt
+volatile boolean bNewThrottleSignalA = false;   // set in the interrupt and read in the loop
+
+// Left-Right steering
+volatile int nThrottleInB = NEUTRAL_THROTTLE_B;    // volatile, we set this in the Interrupt and read it in loop so it must be declared volatile
+volatile unsigned long ulStartPeriodB = 0;  // set in the interrupt
+volatile boolean bNewThrottleSignalB = false;   // set in the interrupt and read in the loop
+
 
 // Colour struct
 struct RGB { 
--- Hyper_Neon/Hyper_Neon.ino
+++ Hyper_Neon/Hyper_Neon.ino
@@ -12,36 +12,6 @@ Config
 // Serial debug
 #define debug_baud 9600
 
-#define weapon_pin 6
-#define t_pin_red 9
-#define t_pin_green 10
-#define t_pin_blue 11
-#define onboard_led_pin 13 // For debugging purposes
-
-#define WEAPON_SIGNAL_IN_A 1       // INTERRUPT 0 = DIGITAL PIN 2 - use the interrupt number in attachInterrupt
-#define WEAPON_SIGNAL_IN_A_PIN 2   // INTERRUPT 0 = DIGITAL PIN 11 - use the PIN number in digitalRead#define NEUTRAL_THROTTLE_A 1460      // this is the duration in microseconds of neutral throttle on an electric RC Car
-#define NEUTRAL_THROTTLE_A 1460      // this is the duration in microseconds of neutral throttle on an electric RC Car
-#define maxForward 1144
-#define maxBack 1820 
-#define DEADZONE_A 200
-
-#define LED_SIGNAL_IN_B 0          // INTERRUPT 1 = DIGITAL PIN 3 - use the interrupt number in attachInterrupt
-#define LED_SIGNAL_IN_B_PIN 3      // INTERRUPT 1 = DIGITAL PIN 3 - use the PIN number in digitalRead
-#define NEUTRAL_THROTTLE_B 1212             // This value may require changing the DX4e (Reading 1420 as neutral
-#define maxLeft 868
-#define maxRight 1760
-#define DEADZONE_B 200
-
-// Forward-Backward steering
-volatile int nThrottleInA = NEUTRAL_THROTTLE_A;    // volatile, we set this in the Interrupt and read it in loop so it must be declared volatile
-volatile unsigned long ulStartPeriodA = 0;  // set in the interrupt
-volatile boolean bNewThrottleSignalA = false;   // set in the interrupt and read in the loop
-
-// Left-Right steering
-volatile int nThrottleInB = NEUTRAL_THROTTLE_B;    // volatile, we set this in the Interrupt and read it in loop so it must be declared volatile
-volatile unsigned long ulStartPeriodB = 0;  // set in the interrupt
-volatile boolean bNewThrottleSignalB = false;   // set in the interrupt and read in the loop
-
 // State variables
 int weapon_state;
 int LED_state;
@@ -78,24 +48,18 @@ void loop() {
     /* Command and control
      * Concept: Get new signals, do 1 cycle of operations
      */
-
-    // Check interrupt flag
-    get_inputs();
     // Defer to controller
     do_cycle();  
 }
 
-/* Collaps pwm signal to a set of discrete states */
+/* Collapse pwm signal to a set of discrete states */
 void get_inputs() {
     /* Weapon control, should be non-blocking */
     if (bNewThrottleSignalA) {
-        // Mode 1
         if (nThrottleInA < NEUTRAL_THROTTLE_A - DEADZONE_A) {
             weapon_state = 0;
-        // Mode 3
         } else if (nThrottleInA > NEUTRAL_THROTTLE_A + DEADZONE_A) {
             weapon_state = 2;
-        // Mode 2
         } else {
             weapon_state = 1;
         }
@@ -103,7 +67,6 @@ void get_inputs() {
     }
     /* LED control, should be blocking */
     if (bNewThrottleSignalB) {
-        
         if (nThrottleInB > NEUTRAL_THROTTLE_B + DEADZONE_B) {
             LED_state = 0;
             generic_gradient();
@@ -163,16 +126,6 @@ void generic_gradient() {
     do_gradient(blue_max, red_max, 4000, 50);  
 }
 
-void seizure(int blinks, int period, RGB colors[], int len) {
-    for (int i = 0; i< blinks; i++) {
-        set_leds(off);
-        digitalWrite(onboard_led_pin, LOW);
-        delay(period);
-        set_leds(colors[random(0, len)]);
-        digitalWrite(onboard_led_pin, HIGH);
-        delay(period);
-    }
-}
 
 /* Generic method for applying a color to LEDs */
 void set_leds(RGB color) {
@@ -181,10 +134,14 @@ void set_leds(RGB color) {
     analogWrite(t_pin_blue, color.b);
 }
 
+/* Basic LED transitions. If they loop with delays, they should get_inputs and break after change.
+
 /* Do a gradient transition from color 1 to color 2*/
 void do_gradient(RGB color_1, RGB color_2, int milli_secs, int step_millis) {
-    // Go from color 1 to color 2 in a number of milli secs
-    // Let me invite you to a game of "The floor is floating point values"
+    // Save current states in case there is a change
+    int weapon_state_now = weapon_state;
+    int LED_state_now = LED_state;
+    
     RGB local_color = {color_1.r, color_1.g, color_1.b};
     set_leds(local_color);
 
@@ -201,6 +158,10 @@ void do_gradient(RGB color_1, RGB color_2, int milli_secs, int step_millis) {
     float step_b = delta_b / total_steps;
 
     while(step < total_steps) {
+        get_inputs();
+        if (weapon_state_now != weapon_state || LED_state_now != LED_state) {
+            break; 
+        }
         local_color.r += step_r;
         local_color.g += step_g;
         local_color.b += step_b;
@@ -212,6 +173,26 @@ void do_gradient(RGB color_1, RGB color_2, int milli_secs, int step_millis) {
     // Compensate for arithmetic / rounding errors
     set_leds(color_2);
 }
+
+
+void seizure(int blinks, int period, RGB colors[], int colors_len) {
+    // Save current states in case there is a change
+    int weapon_state_now = weapon_state;
+    int LED_state_now = LED_state;
+    for (int i = 0; i< blinks; i++) {
+        get_inputs();
+        if (weapon_state_now != weapon_state || LED_state_now != LED_state) {
+            break; 
+        }
+        set_leds(off);
+        digitalWrite(onboard_led_pin, LOW);
+        delay(period);
+        set_leds(colors[random(0, colors_len)]);
+        digitalWrite(onboard_led_pin, HIGH);
+        delay(period);
+    }
+}
+
 
 void do_blink(RGB color_1, RGB color_2, int milli_duration) {
     set_leds(color_2);
